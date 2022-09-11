b74bcdbefc3c4c893746ef16fea493c277033fdc
Inicio <73432509+inisio@users.noreply.github.com>
2022-09-10 19:13:05 -0500

    Add files via upload

---
+++ js/animaciones.js
@@ -0,0 +1,41 @@
+let ubicacion = window.pageYOffset;
+window.onscroll = function(){
+    let ubicacion_actual = window.pageYOffset;
+    if(ubicacion >= ubicacion_actual){
+        document.getElementById('menu').style.top='0';
+    }else{
+        document.getElementById('menu').style.top='-100%';
+    }
+    ubicacion = ubicacion_actual;
+}
+
+const cuadro1 = document.getElementById('imagen_1');
+const cuadro2 = document.getElementById('imagen_2');
+const cuadro3 = document.getElementById('imagen_3');
+const cuadro4 = document.getElementById('imagen_4');
+const cuadro5 = document.getElementById('imagen_5');
+const cuadro6 = document.getElementById('imagen_6');
+const cuadro7 = document.getElementById('imagen_7');
+
+const cargar_cuadro = (entradas, observador) => {
+    entradas.forEach(entrada => {
+        if(entrada.isIntersecting){
+            entrada.target.classList.add('active');
+        }
+        
+    });
+}
+
+const observador = new IntersectionObserver(cargar_cuadro, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2
+});
+
+observador.observe(cuadro1);
+observador.observe(cuadro2);
+observador.observe(cuadro3);
+observador.observe(cuadro4);
+observador.observe(cuadro5);
+observador.observe(cuadro6);
+observador.observe(cuadro7);
