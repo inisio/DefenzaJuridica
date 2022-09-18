0ece6ccf4f2a35f52c57cd33b13322d8be9e6a65
Inicio <73432509+inisio@users.noreply.github.com>
2022-09-17 21:23:06 -0500

    Update animaciones.js

--- js/animaciones.js
+++ js/animaciones.js
@@ -12,10 +12,6 @@ window.onscroll = function(){
 const cuadro1 = document.getElementById('imagen_1');
 const cuadro2 = document.getElementById('imagen_2');
 const cuadro3 = document.getElementById('imagen_3');
-const cuadro4 = document.getElementById('imagen_4');
-const cuadro5 = document.getElementById('imagen_5');
-const cuadro6 = document.getElementById('imagen_6');
-const cuadro7 = document.getElementById('imagen_7');
 
 const cargar_cuadro = (entradas, observador) => {
     entradas.forEach(entrada => {
@@ -35,7 +31,3 @@ const observador = new IntersectionObserver(cargar_cuadro, {
 observador.observe(cuadro1);
 observador.observe(cuadro2);
 observador.observe(cuadro3);
-observador.observe(cuadro4);
-observador.observe(cuadro5);
-observador.observe(cuadro6);
-observador.observe(cuadro7);
