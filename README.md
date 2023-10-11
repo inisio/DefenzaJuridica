df22c896e20129e4c9f24ed8e803f8d04d18bfbb
David-Baca <david.baca.ingenieria@gmail.com>
2023-10-11 15:23:04 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Defensa Juridica Cancun
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,7 @@
+var boton = document.getElementById("activador");
+
+function Menu(){
+    boton.classList.toggle("activado")
+    console.log("hola")
+}
+
